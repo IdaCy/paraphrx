0fd56bea260a355194449098ba3ea40bda508e76
idacy <idacasp@gmail.com>
2025-06-28 11:48:13 +0100

    slurm-adapted run scripts

--- c_assess_inf/src/split_prompt_counts.rs
+++ c_assess_inf/src/split_prompt_counts.rs
@@ -28,6 +28,14 @@ rm c_assess_inf/output/alpaca_prxed/gemma-2-9b-it/answers_slice_100/context_rest
 rm c_assess_inf/output/alpaca_prxed/gemma-2-9b-it/answers_slice_100/context_rest3.json
 */
 
+/*
+cargo countsplit \
+    a_data/alpaca/merge_instructs/all.json \
+    a_data/alpaca/merge_instructs/first300.json \
+    a_data/alpaca/merge_instructs/last200.json \
+    300
+*/
+
 use std::{
     env,
     fs::File,
